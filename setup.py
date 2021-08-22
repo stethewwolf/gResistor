a55a83d733cd0d3dc9c599b4b65e892a58312739
stefano prina <stefano-prina@outlook.it>
2021-08-22 09:03:51 +0200

    added files from sourceforge

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+setup(name='gresistor',
+      version='0.0.1',
+      description      = 'Aici intra descrierea',
+      author           = 'Pop Gheorghe',
+      author_email     = 'pop.gheorghe@rdslink.ro',
+      url              = 'http://www.minipop.org',
+      license          = 'GPL',
+      scripts=['gresistor'],
+      py_modules =['SimpleGladeApp'],
+      data_files = [('share/gresistor',['gresistor.glade']),('share/applications',
+                       ['gresistor.desktop']),('share/gresistor',
+                       ['pixmaps/icon.png'])],
+      )
